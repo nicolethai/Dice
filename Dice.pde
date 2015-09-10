756b81fa8baa6f06b345b54ea54880f1fb636fea
nicolethai <ThaiNicole@gmail.com>
2015-09-10 11:27:34 -0700

    color

--- Dice.pde
+++ Dice.pde
@@ -1,26 +1,29 @@
+int numMouseClicked = 0;
+
 void setup()
 {
 	size(250, 250);
-	background(125);
+	background(212, 212, 202);
 	noLoop();
 }
 void draw()
 {
 	//your code here
 	setup();
-	for (int j = 30; j <= 250; j+=75)
+	for (int j = 35; j <= 250; j+=75)
 	{
-		for (int i = 30; i <= 350; i+=75)
+		for (int i = 35; i <= 250; i+=75)
 		{
 			Die dice = new Die(i, j);
 			dice.roll();
 			dice.show();			
 		}
-		// System.out.println();
 	}
+
 }
 void mousePressed()
 {
+	numMouseClicked++; // include later
 	redraw();
 }
 class Die //models one single dice cube
@@ -37,57 +40,67 @@ class Die //models one single dice cube
 	}
 	void roll()
 	{
-		//your code here		
+		//your code here	
+		// numDots = 1; // for checking if/else and positions.	
 		numDots = (int)((Math.random() * 6) + 1); // generates the random dots for die
 	}
 	void show()
 	{
 		//your code here
 			noStroke();
-			fill(255);
+			fill(75);
+			/********
+			(int)(Math.random()*255)
+			for w,g,b: 
+			w > 125
+			g = 125
+			b < 125
+			********/
 		rect(xPos, yPos, 25, 25, 2.5);
 
-			fill(0);
+			fill(255, 102, 0); // 255, 255, 0
 		/* */ 
+		int dotSize = 4;
 		if (numDots == 1)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
 		}
 		else if (numDots == 2)
 		{
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
 		}
 		else if (numDots == 3)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
 		}
 		else if (numDots == 4)
 		{
-			ellipse(xPos+(25/3), yPos+(25/3), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), 5, 5);
+			ellipse(xPos+(25/3), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), dotSize, dotSize);
 		}
 		else if (numDots == 5)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
-			ellipse(xPos+(25/3), yPos+(25/3), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), dotSize, dotSize);
 		}
 		else if (numDots == 6)
 		{
-			ellipse(xPos+(25/3), yPos+(25/4), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/4)), 5, 5);
-			ellipse(xPos+(25/3), yPos+(3*(25/4)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/4), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/4)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(3*(25/4)), 5, 5);
+			ellipse(xPos+(25/3), yPos+(25/4), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(3*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/4), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(3*(25/4)), dotSize, dotSize);
 		}
 		/* */
+
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -14,29 +14,32 @@ import java.io.IOException;
 
 public class Dice extends PApplet {
 
+int numMouseClicked = 0;
+
 public void setup()
 {
 	size(250, 250);
-	background(125);
+	background(212, 212, 202);
 	noLoop();
 }
 public void draw()
 {
 	//your code here
 	setup();
-	for (int j = 30; j <= 250; j+=75)
+	for (int j = 35; j <= 250; j+=75)
 	{
-		for (int i = 30; i <= 350; i+=75)
+		for (int i = 35; i <= 250; i+=75)
 		{
 			Die dice = new Die(i, j);
 			dice.roll();
 			dice.show();			
 		}
-		System.out.println();
 	}
+
 }
 public void mousePressed()
 {
+	numMouseClicked++; // include later
 	redraw();
 }
 class Die //models one single dice cube
@@ -53,58 +56,68 @@ class Die //models one single dice cube
 	}
 	public void roll()
 	{
-		//your code here		
+		//your code here	
+		// numDots = 1; // for checking if/else and positions.	
 		numDots = (int)((Math.random() * 6) + 1); // generates the random dots for die
 	}
 	public void show()
 	{
 		//your code here
 			noStroke();
-			fill(255);
+			fill(75);
+			/********
+			(int)(Math.random()*255)
+			for w,g,b: 
+			w > 125
+			g = 125
+			b < 125
+			********/
 		rect(xPos, yPos, 25, 25, 2.5f);
 
-			fill(0);
+			fill(255, 102, 0); // 255, 255, 0
 		/* */ 
+		int dotSize = 4;
 		if (numDots == 1)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
 		}
 		else if (numDots == 2)
 		{
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
 		}
 		else if (numDots == 3)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
 		}
 		else if (numDots == 4)
 		{
-			ellipse(xPos+(25/3), yPos+(25/3), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), 5, 5);
+			ellipse(xPos+(25/3), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), dotSize, dotSize);
 		}
 		else if (numDots == 5)
 		{
-			ellipse(xPos+(25/2), yPos+(25/2), 5, 5);
-			ellipse(xPos+(25/3), yPos+(25/3), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/3)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/3), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), 5, 5);
+			ellipse(xPos+(25/2), yPos+(25/2), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/3)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/3), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/3)), dotSize, dotSize);
 		}
 		else if (numDots == 6)
 		{
-			ellipse(xPos+(25/3), yPos+(25/4), 5, 5);
-			ellipse(xPos+(25/3), yPos+(2*(25/4)), 5, 5);
-			ellipse(xPos+(25/3), yPos+(3*(25/4)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(25/4), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(2*(25/4)), 5, 5);
-			ellipse(xPos+(2*(25/3)), yPos+(3*(25/4)), 5, 5);
+			ellipse(xPos+(25/3), yPos+(25/4), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(2*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(25/3), yPos+(3*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(25/4), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(2*(25/4)), dotSize, dotSize);
+			ellipse(xPos+(2*(25/3)), yPos+(3*(25/4)), dotSize, dotSize);
 		}
 		/* */
+
 	}
 }
   static public void main(String[] passedArgs) {
